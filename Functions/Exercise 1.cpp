16b311e4b35468c578123e533ae5f0e9221994b7
Mominsami07 <61017593+Mominsami07@users.noreply.github.com>
2022-01-06 19:27:24 +0100

    Create Exercise 1.cpp
    Define two Functions to print the maximum and the minimum number respectively among three numbers entered by the user.

---
+++ Functions/Exercise 1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int smallest (int first, int second, int third)
+{
+  int min = first;
+  if (second < min)
+    {
+      min = second;
+    }
+  if (third < min)
+    {
+      min = third;
+    }
+  return min;
+}
+
+
+
+
+int largest (int first, int second, int third)
+{
+  int max = first;
+  if (second > max)
+    {
+      max = second;
+    }
+  if (third > max)
+    {
+      max = third;
+    }
+  return max;
+}
+
+int main()
+{
+    int n1,n2,n3;
+    cout<<"Enter 3 integers: ";
+    cin>>n1>>n2>>n3;
+    cout << "\n\n The Smallest number among ( " << n1 << ", " << n2 << ", " << n3 << " ) is : " << smallest(n1,n2,n3);
+    cout << "\n\n The Largest number among ( " << n1 << ", " << n2 << ", " << n3 << " ) is : " << largest(n1,n2,n3);
+
+    return 0;
+}
+
